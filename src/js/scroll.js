020fcec96d3c79b26216b174eaba3aa5687ee98a
unknown <deciobarros97@gmail.com>
2025-03-03 00:40:23 -0300

    first commit

---
+++ src/js/scroll.js
@@ -0,0 +1,51 @@
+window.sr = ScrollReveal ({ reset: true});
+sr.reveal('.area1', {duration: 1500, reset: false, once: true})
+sr.reveal('.area2', {rotate: {x: 0, y: 80, z: 0}, duration: 2500,  reset: false, once: true})
+sr.reveal('.area3', {rotate: {x: 0, y: 80, z: 0}, duration: 2800, reset: false, once: true, delay: 500})
+
+sr.reveal('.card1', 
+    {
+        duration: 1000, 
+        reset: false, 
+        once: true,
+        delay: 500
+    })
+    sr.reveal('.card2', 
+        {
+            duration: 1000, 
+            reset: false, 
+            once: true,
+            delay: 1000
+        })
+
+        sr.reveal('.card3', 
+            {
+                duration: 1000, 
+                reset: false, 
+                once: true,
+                delay: 1500
+            })
+            sr.reveal('.card4', 
+                {
+                    duration: 1000, 
+                    reset: false, 
+                    once: true,
+                    delay: 1800
+                })
+                sr.reveal('.card5', 
+                    {
+                        duration: 1000, 
+                        reset: false, 
+                        once: true,
+                        delay: 2100
+                    })
+                    sr.reveal('.card6', 
+                        {
+                            duration: 1000, 
+                            reset: false, 
+                            once: true,
+                            delay: 2400
+                        })
+
+
+                       
